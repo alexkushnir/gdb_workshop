430c120ae7f2d24b72d6e2dbe610982d2a4eaf49
Alex Kushnir <kushnir.alexander@gmail.com>
2026-03-16 22:49:15 +0200

    Tiny fixes to run_tidy.sh script

--- run_tidy.sh
+++ run_tidy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-find . -type f \( -name "*.cpp" -o -name "*.c" -o -name "*.h" \) \
-       -exec clang-tidy-18 -fix-errors \
-       -config-file=.clang-tidy {} \
-       -- -std=c++23 \;
+find . -type f \( -name "*.cpp" -o -name "*.c" -o -name "*.h" \) -print0 | while IFS= read -r -d '' file; do
+    if [[ "$file" == *.c ]]; then
+        clang-tidy-18 -fix-errors -config-file=.clang-tidy "$file" -- -std=c11
+    else
+        # Treat .cpp and .h as C++
+        clang-tidy-18 -fix-errors -config-file=.clang-tidy "$file" -- -x c++ -std=c++23
+    fi
+done
