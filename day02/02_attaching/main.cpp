fa5c8254d86bdc411b73982c51f611508fe7769b
Alex Kushnir <kushnir.alexander@gmail.com>
2026-04-27 20:32:52 +0300

    Simplify the attach example

--- day02/02_attaching/main.cpp
+++ day02/02_attaching/main.cpp
@@ -1,81 +1,35 @@
-#include <atomic>
 #include <chrono>
 #include <print>
-#include <string>
 #include <thread>
-#include <unistd.h>
-#include <vector>
 
 using namespace std::chrono_literals;
 
-struct Vault
+void PrintFunction()
 {
-    int m_accessCode = 4277;
-    std::string m_secretPayload = "GDB_IS_AWESOME_2026";
-    bool m_isUnlocked = false;
-};
-
-Vault globalVault;
-std::atomic<bool> shouldExit{false};
-
-void DeepComputeLayer3(int id)
-{
-    // This provides a nice deep stack for backtrace practice
-    std::println("[SYSTEM] Processing secure data fragment #{}...", id);
-    std::this_thread::sleep_for(500ms);
-}
-
-void DeepComputeLayer2(int id)
-{
-    DeepComputeLayer3(id);
-}
-void DeepComputeLayer1(int id)
-{
-    DeepComputeLayer2(id);
-}
-
-void SecurityHeartbeat()
-{
-    static int beat = 0;
-    if (globalVault.m_isUnlocked)
+    std::uint64_t counter = 0;
+    while (true)
     {
-        std::println("[SYSTEM] STATUS: !!! COMPROMISED !!! - Secret: {}", globalVault.m_secretPayload);
-    }
-    else
-    {
-        std::println("[SYSTEM] STATUS: SECURE - Heartbeat #{}", ++beat);
+        std::println("{}: {}", std::this_thread::get_id(), ++counter);
+        std::this_thread::sleep_for(1s);
     }
 }
 
-void RunSecurityLayer3()
+void PrintFunctioWrapper()
 {
-    SecurityHeartbeat();
+    PrintFunction();
 }
-void RunSecurityLayer2()
+
+void PrintFunctionWrapperWrapper()
 {
-    RunSecurityLayer3();
-}
-void RunSecurityLayer1()
-{
-    RunSecurityLayer2();
+    PrintFunctioWrapper();
 }
 
 int main()
 {
-    std::println("========================================");
-    std::println("   SECURE DATA VAULT SYSTEM v2.0       ");
-    std::println("========================================");
+    std::println("=========================================");
     std::println("Process ID: {}", getpid());
     std::println("System is running. Attach GDB to inspect.");
-    std::println("----------------------------------------");
-
-    int fragmentId = 0;
-    while (!shouldExit)
-    {
-        SecurityHeartbeat();
-        DeepComputeLayer1(++fragmentId);
-        std::this_thread::sleep_for(1s);
-    }
-
+    std::println("=========================================");
+    PrintFunctionWrapperWrapper();
     return 0;
 }
