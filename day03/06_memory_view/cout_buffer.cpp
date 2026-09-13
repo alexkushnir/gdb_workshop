51ab8497aef777bd35373b06091cc755cce962c1
segsch <ss.adam@gmail.com>
2026-09-13 23:52:31 +0300

    Examine cout buffer

---
+++ day03/06_memory_view/cout_buffer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    	std::cout << "Hello";
+	std::cout << " Goodbye "; 
+	std::cout << " OK " << std::endl;
+	std::cout << "New chars......";
+	// Examine p _IO_2_1_stdout_	
+	
+	return 0;
+}
